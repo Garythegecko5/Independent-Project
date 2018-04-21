5bdb2b7181f698fcd2b1fc33dc4f461d25941b1c
Jonas <garythegecko5@gmail.com>
2018-04-21 13:24:48 -0400

    final draft

--- game_alpha.py
+++ game_alpha.py
@@ -1,60 +1,42 @@
-#HOMEWORK: Fixes List, Prioritize fixes
-#
-# Add Commit Push
 import pygame
 import random
-from menu_components import End_Screen
 from game_components import Player, Bullet, Enemy, Cloud, Blast
+import time
+import os, sys
 
 
-# Initialize Pygame
+# def restart_program():
+#     """Restarts the current program.
+#     Note: this function does not return. Any cleanup action (like
+#     saving data) must be done before calling this function."""
+#     python = sys.executable
+#     os.execl(python, python, *sys.argv)
 pygame.init()
 
-import time
 # Define some colors
 YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 SKY = (76, 203, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-tracker = 0
-player_plane = pygame.image.load('img/planey.bmp')
-ground = pygame.image.load('img/groundy.bmp')
-difficulty = 3
-runner = 0
+WHITE = (255, 255, 255)
+
+# Font
 swanky_small = pygame.font.Font("fonts/Swanky_and_Moo_Moo/SwankyandMooMoo.ttf", 40)
-# --- Functions
-
-
-
-def create_enemy(enemies,sprites):
-    # This represents an enemy
-    enemy = Enemy((709), random.randrange(350))
-
-    # Add the block to the list of objects
-    enemies.add(enemy)
-    sprites.add(enemy)
-
-    return (enemies, sprites)
-
-def update_score(score):
-    text = swanky_small.render("Score: " + str(score), True, BLACK)
-    screen.blit(text, [30,0])
-
+swanky_large = pygame.font.Font("fonts/Swanky_and_Moo_Moo/SwankyandMooMoo.ttf", 120)
 
 def game_intro():
     intro = True
+    image = pygame.image.load('img/menus/start.png')
+    screen.blit(image, [0, 0])
 
     while intro:
-        for event in pygame.event.get():
-            print(event)
+        events = list(pygame.event.get())
+
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
-        image = pygame.image.load('img/menus/start.png')
-        screen.blit(image, [0, 0])
-        clock.tick(60)
 
         start_text = swanky_small.render("Start", True, BLACK)
         end_text = swanky_small.render("Quit", True, BLACK)
@@ -69,7 +51,7 @@ def game_intro():
         if 200 > mouse[0] > 100 and 410 > mouse[1] > 360:
             pygame.draw.rect(screen, YELLOW, (100,360,100,50))
             screen.blit(start_text, [100, 360, 100, 500])
-            for event in pygame.event.get():
+            for event in events:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     intro = False
 
@@ -83,7 +65,7 @@ def game_intro():
         if 600 > mouse[0] > 500 and 410 > mouse[1] > 360:
             pygame.draw.rect(screen, YELLOW, (500, 360, 100, 50))
             screen.blit(end_text, [510, 360, 100, 50])
-            for event in pygame.event.get():
+            for event in events:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pygame.quit()
                     quit()
@@ -91,171 +73,249 @@ def game_intro():
             pygame.draw.rect(screen, RED, (500, 360, 100, 50))
             screen.blit(end_text, [510, 360, 100, 50])
 
+        pygame.display.update()
+        clock.tick(60)
 
 
+def game():
+
+    def create_enemy(enemies, sprites):
+        # This represents an enemy
+        enemy = Enemy(random.randrange(709, 750), random.randrange(350), level)
+
+        # Add the block to the list of objects
+        enemies.add(enemy)
+        sprites.add(enemy)
+        return (enemies, sprites)
+
+    def clear_enemies():
+        for enemy in enemies:
+            enemy.kill()
+
+    def update_score(score):
+        text = swanky_small.render("Score: " + str(score), True, BLACK)
+        screen.blit(text, [30, 0])
+
+    def blit_level(level):
+        if changing_level and pygame.time.get_ticks() < end_time:
+            text = swanky_large.render("LEVEL " + str(level), True, BLACK)
+            screen.blit(text, [150, 150])
+
+
+    level = 1
+    # Clear the screen
+    screen.fill(SKY)
+
+    pygame.mixer.music.load('song.wav')
+    pygame.mixer.music.play(-1)
+
+    # This is a list of every sprite. All blocks and the player block as well.
+    sprites = pygame.sprite.Group()
+
+    # List of each enemy in the game
+    enemies = pygame.sprite.Group()
+
+    # List of each bullet
+    bullets = pygame.sprite.Group()
+
+    # List of each cloud
+    clouds = pygame.sprite.Group()
+
+    start_time = 0
+    end_time = 0
+    changing_level = False
+
+    # Creating 5 Clouds
+    for i in range(5):
+        cloud = Cloud(random.randrange(700), random.randrange(350))
+
+        clouds.add(cloud)
+        sprites.add(cloud)
+
+    # --- Create the sprites
+    enemies, sprites = create_enemy(enemies, sprites)
+
+    # Create a red player block
+
+    player = Player(5, 370)
+    sprites.add(player)
+    score = 0
+    player.alive = True
+    # Loop until the user clicks the close button.
+    done = False
+    while not done:
+
+        # --- Event Processing
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # Fire a bullet if the user clicks the mouse button
+                bullet = Bullet(player.rect.x, player.rect.y)
+
+                # Add the bullet to the lists
+                sprites.add(bullet)
+                bullets.add(bullet)
+
+        for bullet in bullets:
+
+            for enemy in enemies:
+                if pygame.sprite.collide_rect(bullet, enemy):
+                    bullet.kill()
+                    enemy.die()
+                    score += 1
+
+                    for i in range(random.randint(1, 3)):
+                        enemies, sprites = create_enemy(enemies, sprites)
+
+            # Remove the bullet if it flies up off the screen
+
+            if bullet.rect.x > 700:
+                bullet.kill()
+
+        if Enemy.win:
+            player.die()
+            if player.rect.y >= 400:
+                player.alive = True
+                player.kill()
+                done = True
+
+
+        for cloud in clouds:
+            if cloud.destroyed:
+                temp = Cloud(709, random.randrange(350))
+                clouds.add(temp)
+                sprites.add(temp)
+                cloud.kill()
+
+
+
+        if (300 <= score < 600 and level == 1) or \
+                (600 <= score < 1000 and level == 2) or \
+                (1000 <= score < 1300 and level == 3) or \
+                (1300 <= score < 1500 and level == 4):
+            clear_enemies()
+
+            level = level + 1
+            start_time = pygame.time.get_ticks()
+            end_time = start_time + 2500
+            changing_level = True
+
+        if changing_level and pygame.time.get_ticks() >= end_time:
+            changing_level = False
+            enemies, sprites = create_enemy(enemies, sprites)
+
+        # Clear the screen
+        screen.fill(SKY)
+
+        # Call the update() method on all the sprites
+        sprites.update()
+
+        # Draw all the sprites
+        sprites.draw(screen)
+        update_score(score)
+        blit_level(level)
+        pygame.display.update()
+
+        # Go ahead and update the screen with what I've drawn.
+        pygame.display.flip()
+
+        # --- Limit to 60 frames per second
+        clock.tick(60)
+
+
+
+
+
+
+def game_end():
+
+    pygame.event.clear()
+    for sprite in pygame.sprite.Group():
+        sprite.kill()
+
+    looper = False
+    image = pygame.image.load('img/menus/end.png')
+    screen.blit(image, [0, 0])
+
+
+    while not looper:
+
+        events = list(pygame.event.get())
+
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        restart_text = swanky_small.render("Restart", True, BLACK)
+        end_text = swanky_small.render("Quit", True, BLACK)
+        pygame.draw.rect(screen, GREEN, (100, 360, 140, 50))
+        pygame.draw.rect(screen, RED, (500, 360, 100, 50))
+        screen.blit(restart_text, [100, 360, 140, 500])
+        screen.blit(end_text, [510, 360, 100, 50])
+
+        mouse = pygame.mouse.get_pos()
+
+        # Start Box
+        if 240 > mouse[0] > 100 and 410 > mouse[1] > 360:
+            pygame.draw.rect(screen, YELLOW, (100, 360, 140, 50))
+            screen.blit(restart_text, [100, 360, 100, 500])
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    looper = True
+                    #restart_program()
+                    # python = sys.executable
+                    # os.execl(python, python, *sys.argv)
+
+        else:
+            pygame.draw.rect(screen, GREEN, (100, 360, 100, 50))
+            screen.blit(restart_text, [100, 360, 100, 500])
+
+        # End Box
+        if 600 > mouse[0] > 500 and 410 > mouse[1] > 360:
+            pygame.draw.rect(screen, YELLOW, (500, 360, 100, 50))
+            screen.blit(end_text, [510, 360, 100, 50])
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pygame.quit()
+                    quit()
+        else:
+            pygame.draw.rect(screen, RED, (500, 360, 100, 50))
+            screen.blit(end_text, [510, 360, 100, 50])
 
         pygame.display.update()
         clock.tick(60)
 
-# --- Create the window
+
+
+
+
+
+
+
+
+
+
+
+
+# RUNNING THE GAME
+
+
 
 # Set the height and width of the screen
 screen_width = 700
 screen_height = 500
 screen = pygame.display.set_mode([screen_width, screen_height])
 
-# --- Sprite lists
-
-# This is a list of every sprite. All blocks and the player block as well.
-sprites = pygame.sprite.Group()
-
-# List of each enemy in the game
-enemies = pygame.sprite.Group()
-
-# List of each bullet
-bullets = pygame.sprite.Group()
-
-# List of each cloud
-clouds = pygame.sprite.Group()
-
-# List of each powerup
-powerups = pygame.sprite.Group()
-
-# Creating 5 Clouds
-for i in range(5):
-    cloud = Cloud(random.randrange(700), random.randrange(350))
-
-    clouds.add(cloud)
-    sprites.add(cloud)
-
-# --- Create the sprites
-enemies, sprites = create_enemy(enemies, sprites)
-
-# Create a red player block
-player = Player(5, 370)
-sprites.add(player)
-
-# Loop until the user clicks the close button.
-done = False
-
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-score = 0
-
-
-
 game_intro()
-pygame.mixer.music.load('song.wav')
-pygame.mixer.music.play(0)
-# -------- Main Program Loop -----------
-while not done:
-
-
-    # --- Event Processing
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Fire a bullet if the user clicks the mouse button
-            bullet = Bullet(player.rect.x, player.rect.y)
-
-            # Add the bullet to the lists
-            sprites.add(bullet)
-            bullets.add(bullet)
-
-    # --- Game logic
-    # if score == 100:
-    #     enemy = Enemy()
-    #     enemy.speed += 1
-    # if score == 200:
-    #     enemy = Enemy()
-    #     enemy.speed += 1
-    # if score == 300:
-    #     enemy = Enemy()
-    #     enemy.speed += 1
-    # if score == 400:
-    #     enemy = Enemy()
-    #     enemy.speed += 1
-    # if score == 500:
-    #     enemy = Enemy()
-    #     enemy.speed += 1
-    # Calculate mechanics for each bullet
-    for bullet in bullets:
-
-        for enemy in enemies:
-            if pygame.sprite.collide_rect(bullet, enemy):
-                bullets.remove(bullet)
-                sprites.remove(bullet)
-                enemy.die()
-                score += 1
-                tracker += 1
-
-
-
-                for i in range(random.randint(1, difficulty)):
-                    enemies, sprites = create_enemy(enemies, sprites)
-
-        # Remove the bullet if it flies up off the screen
-
-        if bullet.rect.x > 700:
-            bullets.remove(bullet)
-            sprites.remove(bullet)
-
-    if Enemy.win:
-        player.die()
-        if player.rect.y >= 400:
-            end = End_Screen()
-            sprites.add(end)
-
-
-    for cloud in clouds:
-        if cloud.destroyed:
-            temp = Cloud(709, random.randrange(350))
-            clouds.add(temp)
-            sprites.add(temp)
-            clouds.remove(cloud)
-            sprites.remove(cloud)
-
-    # if tracker == 15:
-    #     if runner == 0:
-    #         temp = Blast(709, random.randrange(350))
-    #         runner += 1
-    #         powerups.add(temp)
-    #         sprites.add(temp)
-    # for powerup in powerups:
-    #     if powerup.destroyed:
-    #         temp = Blast(709, random.randrange(350))
-    #         powerups.remove(powerup)
-    #         sprites.remove(powerup)
-
-# Powerup Mechanics
-    bullet = Bullet(player.rect.x, player.rect.y)
-    # for powerup in powerups:
-    #     for player in sprites:
-    #         if pygame.sprite.collide_rect(powerup, player):
-    #             powerups.remove(powerup)
-    #             sprites.remove(powerup)
-    #             print("here")
-
-    # Clear the screen
-    screen.fill(SKY)
-
-    # Call the update() method on all the sprites
-    sprites.update()
-
-    # Draw all the spites
-    sprites.draw(screen)
-    update_score(score)
-    pygame.display.update()
-
-    # Go ahead and update the screen with what I've drawn.
-    pygame.display.flip()
-
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-    pygame.display.set_caption('Game Alpha 2.0')
-
-pygame.quit()
+while True:
+    pygame.display.set_caption('Stratosfight')
+    game()
+    game_end()
+    Enemy.win = False
--- game_components.py
+++ game_components.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 class Player(pygame.sprite.Sprite):
     """ This class represents the Player. """
 
@@ -14,6 +14,12 @@ class Player(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
+    def __str__(self):
+        if self.alive:
+            return "Player is alive!"
+        else:
+            return "Player is dead!"
+
     def update(self):
         """ Update the player's position. """
         if not self.alive:
@@ -24,7 +30,9 @@ class Player(pygame.sprite.Sprite):
             # Set the player y position to the mouse y position
             self.rect.y = pos[1]
 
+
     def die(self):
+
         self.image = pygame.image.load('img/destroyedplane.gif')
         self.alive = False
 
@@ -32,23 +40,49 @@ class Enemy(pygame.sprite.Sprite):
     """ This class represents the block. """
     win = False
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, level):
         # Call the parent class (Sprite) constructor
         super().__init__()
 
+        levels_dists = {
+            0.5: [400, 200, 100, 0, 0],
+            1:   [800, 400, 200, 100, 0],
+            1.5: [400, 800, 400, 200, 100],
+            2:   [200, 400, 800, 400, 400],
+            2.5: [100, 200, 400, 800, 1000],
+            10:  [1,   3,   7,   9, 17]
+        }
+
+        speed_dist = []
+        for speed, dist in levels_dists.items():
+            speed_dist += [speed] * dist[level - 1]
+
+
         self.alive = True
-        self.image = pygame.image.load('img/enemy.gif')
+        self.speed = random.choice(speed_dist)
+        if self.speed == 0.5:
+            self.image = pygame.image.load('img/enemy.gif')
+        elif self.speed == 1:
+            self.image = pygame.image.load('img/enemy2.png')
+        elif self.speed == 1.5:
+            self.image = pygame.image.load('img/enemy3.png')
+        elif self.speed == 2:
+            self.image = pygame.image.load('img/enemy4.png')
+        elif self.speed == 2.5:
+            self.image = pygame.image.load('img/enemy5.png')
+        elif self.speed == 10:
+            self.image = pygame.image.load('img/enemy6.png')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed = 1
-    def update(self):
 
+
+    def update(self):
         if not self.alive:
             self.rect.y += 5
         else:
-            self.rect.x -= 0.5  # Moving Enemy Forward
-            if self.rect.x == self.speed:
+            self.rect.x -= self.speed  # Moving Enemy Forward
+            if self.rect.x < 5:
                 Enemy.win = True
 
     def die(self):
@@ -121,6 +155,4 @@ class Blast(pygame.sprite.Sprite):
         self.rect.x -= 2 #Moving Powerup Forward
         if self.rect.x == 1:
             self.destroyed = True
-class Score(pygame.sprite.Sprite):
-    def __init__(self, x ,y):
-        super().__init__()
+
